2f85bda43d7db669753ed73d2fcc0391c1c73010
themoffat28 <bthemoffat@gmail.com>
2017-01-27 08:05:09 -0700

    Initial Commit

---
+++ Second C++ Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Second C++ Project
+//
+//  Created by Moffat, Braeden on 1/27/17.
+//  Copyright © 2017 Moffat, Braeden. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
